d50bf1f0bb1bd311d734aff7f1bfad7a3065a0d5
AntonKuchinskiy <anton.kuchinskiy@wolterskluwer.com>
2022-03-24 11:31:09 +0100

    Inventory service

---
+++ Play.Inventory/src/Play.Inventory.Service/CatalogClient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Play.Inventory.Service.Dtos.Clients;
+using Polly;
+using Polly.Timeout;
+
+namespace Play.Inventory.Service
+{
+    public static class CatalogClientExtensions
+    {
+        public static void AddCatalogClient(this IServiceCollection services)
+        {
+            Random jitterer = new Random();
+
+            services.AddHttpClient<CatalogClient>(client =>
+                {
+                    client.BaseAddress = new Uri("https://localhost:5001");
+                })
+                .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+                    5,
+                    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                                    + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
+                    onRetry: (_, timespan, retryAttempt) =>
+                    {
+                        var serviceProvider = services.BuildServiceProvider();
+                        serviceProvider.GetService<ILogger<CatalogClient>>()?
+                            .LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
+                    }
+                ))
+                .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+                    3,
+                    TimeSpan.FromSeconds(15),
+                    onBreak: (_, timespan) =>
+                    {
+                        var serviceProvider = services.BuildServiceProvider();
+                        serviceProvider.GetService<ILogger<CatalogClient>>()?
+                            .LogWarning($"Opening the circuit for {timespan.TotalSeconds} seconds...");
+                    },
+                    onReset: () =>
+                    {
+                        var serviceProvider = services.BuildServiceProvider();
+                        serviceProvider.GetService<ILogger<CatalogClient>>()?
+                            .LogWarning($"Closing the circuit...");
+                    }
+                ))
+                .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
+        }
+    }
+}
---
+++ Play.Inventory/src/Play.Inventory.Service/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Play.Inventory.Service.Dtos.Entities;
+
+namespace Play.Inventory.Service.Dtos
+{
+    public static class Extensions
+    {
+        public static InventoryItemDto AsDto(this InventoryItem item, string name, string description)
+        {
+            return new InventoryItemDto(item.CatalogItemId, name, description, item.Quantity, item.AcquiredDate);
+        }
+    }
+}
